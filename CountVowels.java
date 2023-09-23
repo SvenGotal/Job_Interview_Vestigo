08b5303c54c975df04dff628d37fafe46630a608
Sven_Gotal <sven.gotal@gmail.com>
2023-09-23 09:05:31 +0200

    Apply changes to public void main
    Apply required changes in the console application, VowelCounter class was modified and change version from 1.0 to 1.0.1

---
+++ CountVowels.java
@@ -0,0 +1,32 @@
+
+
+package com.vestigo.svengotal;
+
+import java.io.FileNotFoundException;
+
+/**
+ * <h1>Count Vowels application</h1>
+ * <p>CountVowels application counts vowels from a given .txt or .xml files. 
+ * The result is displayed to the standard output in terminal.</p>
+ * 
+ * @author Sven Gotal
+ * @version 1.0.1
+ * @since 18.09.2023
+ */
+public class CountVowels {
+
+	public static void main(String[] args) {
+		
+		try {
+			VowelCounter vc = new VowelCounter(args[0]);
+			vc.processFile();
+			System.out.println("Found: " + vc.giveVowels() + " vowels.");
+			System.out.println("Found: " + vc.giveConsonants() + " consonants.");
+			
+		} catch (FileNotFoundException e) {
+			
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
